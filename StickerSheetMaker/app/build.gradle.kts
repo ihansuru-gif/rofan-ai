d26219594fc1493832bb742481e60d57967be927
ihansuru-gif <ihansuru@gmail.com>
2026-08-23 19:24:08 +0900

    Add StickerSheetMaker app Gradle build

---
+++ StickerSheetMaker/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.oai.stickersheet"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.oai.stickersheet"
+        minSdk = 24
+        targetSdk = 37
+        versionCode = 4
+        versionName = "0.3.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core:1.17.0")
+    testImplementation("junit:junit:4.13.2")
+    implementation("com.google.android.gms:play-services-mlkit-subject-segmentation:16.0.0-beta1")
+}
